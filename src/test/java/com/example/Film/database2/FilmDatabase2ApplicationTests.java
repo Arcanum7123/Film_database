39d9fee3e795bc25cd8bd8a83b1e041b147a6d7c
Jacob <jacobcc16@gmail.com>
2024-02-01 15:43:09 +0000

    Commit including first test

--- src/test/java/com/example/Film/database2/FilmDatabase2ApplicationTests.java
+++ src/test/java/com/example/Film/database2/FilmDatabase2ApplicationTests.java
@@ -1,13 +1,46 @@
 package com.example.Film.database2;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
 
-@SpringBootTest
+import java.util.Optional;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+
+@WebMvcTest(controllers = {FilmDatabase2Application.class})
 class FilmDatabase2ApplicationTests {
+	@Autowired
+	private MockMvc mockMvc;
 
+	@MockBean
+	private ActorRepository actorRepo;
+	@MockBean
+	private FilmRepository filmRepo;
+	@MockBean
+	private CategoryRepository categoryRepo;
+	@MockBean
+	private FilmCategoryRepository filmCatRepo;
+	@MockBean
+	private LanguageRepository languageRepo;
+	@MockBean
+	private FilmActorRepository filmActorRepo;
+
+
+	Actor barry = new Actor(18, "BAROLD", "GIBBONS");
 	@Test
-	void contextLoads() {
+	public void testFindActorByID() throws Exception {
+		when(actorRepo.findById(barry.getActorID())).thenReturn(Optional.of(barry));
+		this.mockMvc.perform(get("/home/actor/18").accept(MediaType.APPLICATION_JSON)).andExpect(jsonPath("$.actorID").value(18))
+				.andExpect(jsonPath("$.firstName").value("BAROLD")).andExpect(jsonPath("$.lastName").value("GIBBONS"));
 	}
 
 }
